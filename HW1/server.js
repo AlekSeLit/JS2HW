a8c6b878caae54d2d6c0977c9840145877183d87
AlekSeLit <AlekSeLitLoot@gmail.com>
2020-11-12 00:10:22 +0300

    server

--- HW1/server.js
+++ HW1/server.js
@@ -6,28 +6,28 @@ const server = http.createServer((req, res) => {
 for (let i=0;i<=5;i++) {
     switch (req.url) {
         case "/":
-          body = fs.readFileSync(`./shop/index.html`);
+          body = fs.readFileSync(`./HW1/shop/index.html`);
           break;
         case "/main.js":
-          body = fs.readFileSync(`./shop/main.js`);
+          body = fs.readFileSync(`./HW1/shop/main.js`);
           break;
         case "/style.css":
-          body = fs.readFileSync(`./shop/style.css`);
+          body = fs.readFileSync(`./HW1/shop/style.css`);
           break;
         case "/fonts/BebasNeue.ttf":
-          body = fs.readFileSync(`./shop/fonts/BebasNeue.ttf`);
+          body = fs.readFileSync(`./HW1/shop/fonts/BebasNeue.ttf`);
           break;
         case "/fonts/Lobster.ttf":
-          body = fs.readFileSync(`./shop/fonts/Lobster.ttf`);
+          body = fs.readFileSync(`./HW1/shop/fonts/Lobster.ttf`);
           break;
         case "/fonts/opensans.ttf":
-          body = fs.readFileSync(`./shop/fonts/opensans.ttf`);
+          body = fs.readFileSync(`./HW1/shop/fonts/opensans.ttf`);
           break;
         case `/img/Layer_${i}.png`:
-          body = fs.readFileSync(`./shop/img/Layer_${i}.png`);
+          body = fs.readFileSync(`./HW1/shop/img/Layer_${i}.png`);
           break;
         case `/img/catalog/id${i}.png`:
-           body = fs.readFileSync(`./shop/img/catalog/id${i}.png`);
+           body = fs.readFileSync(`./HW1/shop/img/catalog/id${i}.png`);
            break;
       }
 }
