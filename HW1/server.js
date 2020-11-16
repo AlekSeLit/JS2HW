2b0870fca0b0dd46026de568ed7e7e27159950d8
AlekSeLit <AlekSeLitLoot@gmail.com>
2020-11-16 12:46:02 +0300

    change started on server

--- HW1/server.js
+++ HW1/server.js
@@ -2,38 +2,18 @@ const http = require("http");
 const fs = require("fs");
 const server = http.createServer((req, res) => {
   console.log(req.url);
-  let body;
-for (let i=0;i<=5;i++) {
-    switch (req.url) {
-        case "/":
-          body = fs.readFileSync(`./HW1/shop/index.html`);
-          break;
-        case "/main.js":
-          body = fs.readFileSync(`./HW1/shop/main.js`);
-          break;
-        case "/style.css":
-          body = fs.readFileSync(`./HW1/shop/style.css`);
-          break;
-        case "/fonts/BebasNeue.ttf":
-          body = fs.readFileSync(`./HW1/shop/fonts/BebasNeue.ttf`);
-          break;
-        case "/fonts/Lobster.ttf":
-          body = fs.readFileSync(`./HW1/shop/fonts/Lobster.ttf`);
-          break;
-        case "/fonts/opensans.ttf":
-          body = fs.readFileSync(`./HW1/shop/fonts/opensans.ttf`);
-          break;
-        case `/img/Layer_${i}.png`:
-          body = fs.readFileSync(`./HW1/shop/img/Layer_${i}.png`);
-          break;
-        case `/img/catalog/id${i}.png`:
-           body = fs.readFileSync(`./HW1/shop/img/catalog/id${i}.png`);
-           break;
-      }
-}
+  const publicPath = `./shop`;
+  let body = null;
+  try {
+    body = fs.readFileSync(`${publicPath}${req.url}`);
+  } catch (e) {
+    console.log(e);
+    body = fs.readFileSync(`${publicPath}/index.html`);
+  }
   res.end(body);
 });
 
-server.listen(process.env.PORT || 3000);
+const port = process.env.PORT || 3333;
+server.listen(port);
 
-console.log("Server started");
+console.log(`Server started on port ${port}`);
